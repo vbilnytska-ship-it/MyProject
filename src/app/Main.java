2b9180916ed900768ddb46cde1fae8ec002e2f04
Valeriia Bilnytska <vbilnytska@ubki.ua>
2025-12-16 21:45:01 +0200

    Update sales calculation and output

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,41 +1,49 @@
 
 
 public class Main {
+
+    static String productName;
+    static int quantity;
+    static double price;
+    static int days;
+
+    static double totalSales;
+    static double salesByDay;
+
+    private static final String CURRENCY = "EUR";
+
     public static void main(String[] args) {
 
-        // Всі змінні для замоілення
-        int orderNo;
-        String client;
-        String product;
-        double price;
-        String address;
+        // Товар 1
+        productName = "smartphone";
+        quantity = 5;
+        price = 2430.68;
+        days = 5;
 
-        // Замовлення номер 1
-        orderNo   = 1;
-        client    = "Alice";
-        product   = "smartphone";
-        price     = 305.99;
-        address   = "Moon Street, 10";
+        totalSales = quantity * price;
+        salesByDay = totalSales / days;
 
-        System.out.println(
-                "Order No " + orderNo + " Client: " + client + "\n" +
-                        "Product: " + product + "\n" +
-                        "price EUR " + price + "\n" +
-                        "Address: " + address + "."
+        System.out.printf(
+                "Product No 1: %s,%n" +
+                        "total sales for %d days is %s %.2f,%n" +
+                        "sales by day is %s %.2f.%n",
+                productName, days, CURRENCY, totalSales, CURRENCY, salesByDay
         );
 
-        // Замовлення номер 2
-        orderNo   = 2;
-        client    = "Tom";
-        product   = "laptop";
-        price     = 570.95;
-        address   = "Terra Street, 17";
+        // Товар 2
+        productName = "laptop";
+        quantity = 7;
+        price = 1498.12;
+        days = 7;
 
-        System.out.println(
-                "Order No " + orderNo + " Client: " + client + "\n" +
-                        "Product: " + product + "\n" +
-                        "price EUR " + price + "\n" +
-                        "Address: " + address + "."
+        totalSales = quantity * price;
+        salesByDay = totalSales / days;
+
+        System.out.printf(
+                "Product No 2: %s,%n" +
+                        "total sales for %d days is %s %.2f,%n" +
+                        "sales by day is %s %.2f.%n",
+                productName, days, CURRENCY, totalSales, CURRENCY, salesByDay
         );
     }
 }
