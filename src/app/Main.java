d6d05bfca33145ae8af76635fa4416105b532d3a
Valeriia Bilnytska <vbilnytska@ubki.ua>
2025-12-30 11:36:25 +0200

    Оновлені дані в коді

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,29 +5,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Змінна для збереження суми доходу
-        double income = 10000;
+        int sum = 0;
 
-        // Змінна для збереження суми податку
-        double tax;
-
-        // Якщо дохід менший або дорівнює 10000
-        // податок становить 2,5%
-        if (income <= 10000) {
-            tax = income * 0.025;
-
-            // Якщо дохід більший за 10000, але не перевищує 25000
-            // податок становить 4,3%
-        } else if (income <= 25000) {
-            tax = income * 0.043;
-
-            // Якщо дохід більший за 25000
-            // податок становить 6,7%
-        } else {
-            tax = income * 0.067;
+        for (int i = 1; i <= 6; i++) {
+            sum = sum + i;
+            System.out.println(i + ") Num is " + i + ", sum is " + sum);
         }
 
-        System.out.println("Income = " + income);
-        System.out.println("Tax = " + tax);
+        System.out.println("------------------------");
+        System.out.println("Sum of numbers is " + sum);
     }
 }
