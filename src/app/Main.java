8575a98cb99ebe9cb6aca19fc4539cfbf6f8ddcb
Valeriia Bilnytska <vbilnytska@ubki.ua>
2026-01-29 21:10:01 +0200

    Домашня робота 13

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,19 +1,26 @@
 package app;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        // створюємо співробітника
-        Employee employee = new Employee(
-                "Більницька Валерія Володимирівна",
-                "Менеджер",
-                "valeria@example.com",
-                "+380501234567",
-                27
-        );
+        Shape[] shapes = new Shape[] {
+                new Circle(5),
+                new Square(4),
+                new Triangle(6, 3)
+        };
 
-        // створюємо авто
-        Car car = new Car();
-        car.start();
+        double totalArea = calculateTotalArea(shapes);
+        System.out.println("Сумарна площа всіх фігур: " + totalArea);
+    }
+
+    public static double calculateTotalArea(Shape[] shapes) {
+        double sum = 0;
+
+        for (Shape shape : shapes) {
+            sum += shape.getArea(); // поліморфізм
+        }
+
+        return sum;
     }
 }
---
+++ src/app/Square.java
@@ -0,0 +1,15 @@
+package app;
+
+public class Square implements Shape {
+
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double getArea() {
+        return side * side;
+    }
+}
